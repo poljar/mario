4467417a9aa7cef2921e941d0fd192a2e3c3a92c
poljar (Damir Jelić) <poljarinho@gmail.com>
2015-02-11 18:26:36 +0100

    Add experimental config.

--- mario.py
+++ mario.py
@@ -4,156 +4,121 @@
 # found in the LICENSE file.
 
 import os
+import re
 import sys
 import argparse
 import tempfile
 import mimetypes
 import subprocess
+import configparser
 import logging as log
 import urllib.request
+
+from functools import reduce
 from urllib.error import HTTPError, URLError
 from urllib.parse import urlparse
 
-def github_translation(url):
-    url = url.replace('github.com', 'raw.githubusercontent.com')
-    url = url.replace('blob/', '')
-    return url
 
-VIDEO_STREAMER      = 'livestreamer'
+def domain_is_func(data, arguments, match_group):
+    url = urlparse(data)
 
-VIDEO_URLS          = ('youtube.com', 'youtu.be', 'www.youtube.com')
-STREAM_URLS         = ('www.twitch.tv', 'twitch.tv')
-TRANSLATION_RULES   = {
-    'github.com' : github_translation,
+    return url.netloc in arguments, match_group
+
+
+def content_is_func(data, arguments, match_group):
+    t, _ = mimetypes.guess_type(data)
+
+    try:
+        return t in arguments, match_group
+    except TypeError:
+        return False, match_group
+
+
+def type_is_func(data, arguments, match_group):
+    url = urlparse(data)
+    is_url = bool(url.scheme)
+
+    if arguments == 'text' and not is_url:
+        return True, match_group
+    elif arguments == 'url' and is_url:
+        return True, match_group
+    else:
+        return False, match_group
+
+
+def data_is_func(data, arguments, match_group):
+    return data in arguments, match_group
+
+
+def data_match_func(data, arguments, match_group):
+    m = None
+
+    print(data)
+    for pattern in arguments.split('\n'):
+        pattern = '(' + pattern + ')'
+        m = re.search(pattern, data)
+
+        if not m:
+            return False, ()
+
+    return True, m.groups()
+
+
+def data_rewrite_func(data, arguments, match_group):
+    f = lambda acc, r: acc.replace(*(r.split(',', 2)))
+
+    return reduce(f, arguments.split('\n'), data)
+
+
+def plumber_open_func(data, arguments, match_group):
+    tmp = arguments.format(*match_group, data=data)
+    print(tmp)
+    return subprocess.call(tmp.split())
+
+
+match_rules = {
+        'type is'      : type_is_func,
+        'data is'      : domain_is_func,
+        'domain is'    : domain_is_func,
+        'content is'   : content_is_func,
+        'data matches' : data_match_func,
 }
 
-USER_AGENT = 'Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/20100101 Firefox/36.0'
+action_rules = {
+        'data rewrite' : data_rewrite_func,
+        'plumber open' : plumber_open_func,
+}
 
 
-def launch_command(command, args):
-    if type(args) is str:
-        args = [args]
+def handle_rules(data, config):
+    rule_matched = False
 
-    return subprocess.call([command] + args)
+    for rule in config.sections():
 
+        match_group = ()
+        options = config.options(rule)
 
-def launch_browser(url):
-    browser = os.getenv('BROWSER')
+        match_options = list(filter((lambda opt: opt in match_rules),
+                            options))
+        action_options = list(filter((lambda opt: opt in action_rules),
+                            options))
 
-    if not browser:
-        browser = 'rifle'
+        for opt in match_options:
+            f = match_rules[opt]
+            res, match_group = f(data, config.get(rule, opt), match_group)
 
-    return launch_command(browser, url)
+            if not res:
+                rule_matched = False
+                break
 
+            rule_matched = True
 
-def is_terminal():
-    # Check if stdin, stdout and stderr are connected to a terminal.
-    return sys.stdout.isatty() and sys.stdin.isatty() and sys.stderr.isatty()
+        if rule_matched:
+            for opt in action_options:
+                f = action_rules[opt]
+                data = f(data, config.get(rule, opt), match_group)
 
-
-def launch_editor(url):
-    editor = os.getenv('EDITOR')
-
-    if not editor:
-        editor = 'rifle'
-
-    if editor in ('vi', 'vim', 'neovim', 'nano', 'joe') and not is_terminal():
-        term = os.getenv('TERMCMD')
-
-        if term and term.endswith('termite'):
-            file_name = download_file(url)
-
-            if file_name:
-                return launch_command('termite', ['-e', editor + ' ' + file_name])
-            else:
-                return -1
-
-        return download_launch_command('rifle', url, '-f t')
-
-    return download_launch_command('rifle', url)
-
-
-def download_file(url):
-    opener = urllib.request.build_opener()
-    opener.addheaders = [('User-agent', USER_AGENT)]
-
-    tmp_dir = tempfile.gettempdir()
-
-    try:
-        with tempfile.NamedTemporaryFile(prefix='plumber-', dir=tmp_dir, delete=False) as f:
-            f.write(opener.open(url).read())
-            return f.name
-    except OSError as e:
-        log.debug('Error downloading file: ' + str(e))
-
-        return None
-
-
-def download_launch_command(command, url, args=None):
-    file_name = download_file(url)
-
-    if not file_name:
-        return -1
-
-    if args:
-        if type(args) is str:
-            args = [args] + [file_name]
-        else:
-            args = args + [file_name]
-    else:
-        args = file_name
-
-    return launch_command(command, args)
-
-
-def handle_mime(url, mime_type):
-    if mime_type.startswith('video/'):
-        return launch_command('rifle', url)
-
-    elif mime_type.startswith('image/'):
-        return download_launch_command('rifle', url)
-
-    elif mime_type == 'application/pdf':
-        return download_launch_command('rifle', url)
-
-    elif mime_type.startswith('text/') and mime_type != 'text/html':
-        return launch_editor(url)
-
-    else:
-        return launch_browser(url)
-
-
-def lookup_content_type(url):
-    request = urllib.request.Request(url=url, method='HEAD')
-
-    try:
-        request = urllib.request.urlopen(request)
-        response = request.getheader('Content-Type')
-    except (HTTPError, URLError) as e:
-        log.debug('Error looking up content type: ' + str(e))
-
-        return None, None
-
-    if ';' in response:
-        content_type, encoding = response.split(';', maxsplit=1)
-        return content_type, encoding.strip()
-
-    return response, None
-
-
-def find_mime_type(url):
-    path = url.path
-    url = url.geturl()
-
-    mime_type, encoding = mimetypes.guess_type(url)
-
-    if not mime_type:
-        mime_type, encoding = mimetypes.guess_type(path)
-
-    if not mime_type:
-        mime_type, encoding = lookup_content_type(url)
-
-    return mime_type
+            break
 
 
 def main():
@@ -161,7 +126,7 @@ def main():
 
     parser.add_argument('-v', '--verbose', help='turn on verbose mode',
                         action='store_true')
-    parser.add_argument('URL', help='URL to handle.', type=str)
+    parser.add_argument('data', help='Data to handle', type=str)
 
     args = parser.parse_args()
 
@@ -170,26 +135,12 @@ def main():
     else:
         log.basicConfig(format='%(levelname)s: %(message)s')
 
-    url = urlparse(args.URL)
-    url_string = url.geturl()
+    config = configparser.ConfigParser()
+    config.read('example.ini')
 
-    if url.netloc in (VIDEO_URLS):
-        return launch_command('mpv', url_string)
+    config.remove_section('mario')
 
-    elif url.netloc in (STREAM_URLS):
-        return launch_command(VIDEO_STREAMER, url_string)
-
-    mime_type = find_mime_type(url)
-
-    if mime_type and mime_type != 'text/html':
-        for site, rule in TRANSLATION_RULES.items():
-            if site in url.netloc:
-                url_string = rule(url_string)
-
-        return handle_mime(url_string, mime_type)
-    else:
-        return launch_browser(url_string)
-
+    handle_rules(args.data, config)
 
 if __name__ == '__main__':
     main()
