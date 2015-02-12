4148706f105602bc9f71f732d54ec8b2c6b785f8
poljar (Damir Jelić) <poljarinho@gmail.com>
2015-02-12 01:03:27 +0100

    Make mario even more plumber like.

--- mario.py
+++ mario.py
@@ -19,78 +19,89 @@ from urllib.error import HTTPError, URLError
 from urllib.parse import urlparse
 
 
-def domain_is_func(data, arguments, match_group):
-    url = urlparse(data)
-
-    return url.netloc in arguments, match_group
+def domain_is_func(msg, arguments, match_group):
+    return msg['netloc'] in arguments, msg, match_group
 
 
-def content_is_func(data, arguments, match_group):
-    t, _ = mimetypes.guess_type(data)
+def content_is_func(msg, arguments, match_group):
+    t, _ = mimetypes.guess_type(msg['data'])
 
     try:
-        return t in arguments, match_group
+        return t in arguments, msg, match_group
     except TypeError:
-        return False, match_group
+        return False, msg, match_group
 
 
-def type_is_func(data, arguments, match_group):
-    url = urlparse(data)
+def type_is_func(msg, arguments, match_group):
+    url = urlparse(msg['data'])
     is_url = bool(url.scheme)
 
     if arguments == 'text' and not is_url:
-        return True, match_group
+        return True, msg, match_group
     elif arguments == 'url' and is_url:
-        return True, match_group
+        msg['netloc'] = url.netloc
+        msg['netpath'] = url.path
+        return True, msg, match_group
     else:
-        return False, match_group
+        return False, msg, match_group
 
 
-def data_is_func(data, arguments, match_group):
-    return data in arguments, match_group
+def data_is_func(msg, arguments, match_group):
+    return msg['data'] in arguments, msg, match_group
 
 
-def data_match_func(data, arguments, match_group):
+def data_match_func(msg, arguments, match_group):
     m = None
 
-    print(data)
+    print(msg)
     for pattern in arguments.split('\n'):
-        pattern = '(' + pattern + ')'
-        m = re.search(pattern, data)
+        m = re.search(pattern, msg['data'])
 
         if not m:
-            return False, ()
+            return False, msg, ()
 
-    return True, m.groups()
+    if m.groups():
+        return True, msg, m.groups()
+    else:
+        return True, msg, (m.group(),)
 
 
-def data_rewrite_func(data, arguments, match_group):
+def data_rewrite_func(msg, arguments, match_group):
     f = lambda acc, r: acc.replace(*(r.split(',', 2)))
 
-    return reduce(f, arguments.split('\n'), data)
+    msg['data'] = reduce(f, arguments.split('\n'), msg['data'])
+
+    return True, msg, match_group
 
 
-def plumber_open_func(data, arguments, match_group):
-    tmp = arguments.format(*match_group, data=data)
+def plumber_open_func(msg, arguments, match_group):
+    print(msg, match_group)
+    tmp = arguments.format(*match_group, **msg)
     print(tmp)
-    return subprocess.call(tmp.split())
+#    return subprocess.call(tmp.split())
 
+def arg_is_func(msg, arguments, match_group):
+    arg, check = arguments.split()
+
+    if arg.format(*match_group, **msg) == check:
+        return True, msg, match_group
 
 match_rules = {
+        'arg is'       : arg_is_func,
         'type is'      : type_is_func,
         'data is'      : domain_is_func,
         'domain is'    : domain_is_func,
         'content is'   : content_is_func,
         'data matches' : data_match_func,
+        'data rewrite' : data_rewrite_func,
 }
 
 action_rules = {
-        'data rewrite' : data_rewrite_func,
         'plumber open' : plumber_open_func,
 }
 
 
-def handle_rules(data, config):
+def handle_rules(msg, config):
     rule_matched = False
 
     for rule in config.sections():
@@ -98,14 +109,12 @@ def handle_rules(data, config):
         match_group = ()
         options = config.options(rule)
 
-        match_options = list(filter((lambda opt: opt in match_rules),
-                            options))
-        action_options = list(filter((lambda opt: opt in action_rules),
-                            options))
+        match_options  = [opt for opt in options if opt in match_rules]
+        action_options = [opt for opt in options if opt in action_rules]
 
         for opt in match_options:
             f = match_rules[opt]
-            res, match_group = f(data, config.get(rule, opt), match_group)
+            res, msg, match_group = f(msg, config.get(rule, opt), match_group)
 
             if not res:
                 rule_matched = False
@@ -116,7 +125,7 @@ def handle_rules(data, config):
         if rule_matched:
             for opt in action_options:
                 f = action_rules[opt]
-                data = f(data, config.get(rule, opt), match_group)
+                msg = f(msg, config.get(rule, opt), match_group)
 
             break
 
@@ -126,7 +135,7 @@ def main():
 
     parser.add_argument('-v', '--verbose', help='turn on verbose mode',
                         action='store_true')
-    parser.add_argument('data', help='Data to handle', type=str)
+    parser.add_argument('msg', help='message to handle', type=str)
 
     args = parser.parse_args()
 
@@ -140,7 +149,7 @@ def main():
 
     config.remove_section('mario')
 
-    handle_rules(args.data, config)
+    handle_rules({'data' : args.msg}, config)
 
 if __name__ == '__main__':
     main()
