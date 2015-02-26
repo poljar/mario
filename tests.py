667f4d4d3e748fd2ec4153f664c99e6598b60081
poljar (Damir Jelić) <poljar@termina.org.uk>
2015-02-26 18:09:41 +0100

    Add unit testing.

---
+++ tests.py
@@ -0,0 +1,86 @@
+import unittest
+
+from parser import make_parser, parse_rule_string
+
+
+rule_test1 = '''[test]
+arg matches {data} regex_string
+plumb open firefox'''
+
+rule_res1 = [
+                ['test', (
+                    [
+                        ['arg', 'matches', '{data}', ['regex_string']]
+                    ],
+                    [
+                        ['plumb', 'open', 'firefox']]
+                    )
+                ]
+            ]
+
+
+rule_test2 = '''[test]
+arg matches {data} regex_string
+                   regex_inbetween
+plumb open firefox'''
+
+rule_res2 = [
+                ['test', (
+                    [
+                        ['arg', 'matches', '{data}', ['regex_string', 'regex_inbetween']]], \
+                    [
+                        ['plumb', 'open', 'firefox']]
+                    )
+                ]
+            ]
+
+
+rule_test3 = '''[test]
+arg matches {data} regex_string
+                   regex_inbetween
+plumb open firefox
+[test2]
+arg is {data} something
+plumb open echo {data}'''
+
+rule_res3 = [
+                ['test', (
+                    [
+                        ['arg', 'matches', '{data}', ['regex_string', 'regex_inbetween']]
+                    ],
+                    [
+                        ['plumb', 'open', 'firefox']
+                    ]
+                    )
+                ], \
+                ['test2', (
+                    [
+                        ['arg', 'is', '{data}', ['something']]
+                    ],
+                    [
+                        ['plumb', 'open', 'echo {data}']
+                    ]
+                    )
+                ]
+            ]
+
+
+class ParserTest(unittest.TestCase):
+    def test_simple_rule(self):
+        parser = make_parser()
+        result = parse_rule_string(parser, rule_test1)
+        self.assertEqual(result, rule_res1)
+
+    def test_multiple_match_args(self):
+        parser = make_parser()
+        result = parse_rule_string(parser, rule_test2)
+        self.assertEqual(result, rule_res2)
+
+    def test_multiple_rules(self):
+        parser = make_parser()
+        result = parse_rule_string(parser, rule_test3)
+        self.assertEqual(result, rule_res3)
+
+
+if __name__ == '__main__':
+        unittest.main()
